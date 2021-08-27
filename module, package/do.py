5761bd57f947075e4ad2121d0d2e21415aa1a3a0
iruyj <jeon1310@gmail.com>
2021-08-27 15:53:02 +0900

    module commit

---
+++ module, package/do.py
@@ -0,0 +1,5 @@
+import my_module
+my_module.nara_rose()
+
+if '__init__' == '__main__':
+    print('내부')
---
+++ module, package/my_module.py
@@ -0,0 +1,10 @@
+def nara_rose():
+    print('게임 FPS')
+
+def hoho():
+    print('마라탕')
+
+date = '2021.8.27'
+print(date)
+nara_rose()
+hoho()
