f398774418f0e098c5eaa62649f566a76d181c59
Jyuri <jeon1310@gmail.com>
2021-10-01 14:53:33 +0900

    load text 3가지 방법

---
+++ file handler/load_text.py
@@ -0,0 +1,21 @@
+print('전체 한꺼번에 읽기')
+f = open('text.txt', 'r', encoding='utf-8')  # utf-8 : 읽는것과 쓰는것의 형식이 같아야함
+data = f.read()
+f.close()
+
+print(data)
+print('한줄 씩 읽기')
+f = open('text.txt', 'r', encoding='utf-8')
+while True:
+    line = f.readline()  # line: 나 옥색\n
+    if line == '' :     #끝나는 지점 == 빈칸
+        break
+    print(line.rstrip())  # 글옆의 빈칸 탭,\n 등이 지워짐
+f.close()
+
+print('전체을 한꺼번에 읽고, 줄 별로 리스트')
+f = open('text.txt','r', encoding='utf-8')
+lines = f.readlines()
+f.close()
+for line in lines:
+    print(line.rstrip())
--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,8 +1,9 @@
 # 파일을 실제로 열기
-f = open('text.txt','w')
+f = open('text.txt','w', encoding='utf-8')
 
 f.write('나 ')
-f.write('좋아하는 색 : ')
-f.write('옥색 ')
+f.write('옥색\n')
+f.write('남\n')
+f.write('남색')
 
 f.close()   #닫기
