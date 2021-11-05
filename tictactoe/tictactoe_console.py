4433e64ea1f1eb08af227194eb2088dee452a270
Jyuri <jeon1310@gmail.com>
2021-11-05 15:35:31 +0900

    tictactoe_console.py 완성

--- tictactoe/tictactoe_console.py
+++ tictactoe/tictactoe_console.py
@@ -1,3 +1,32 @@
+from tictactoe.tictactoe_gameengine import TictactoeGameEngine
+
+
+class TictactoeConsole:
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+
+    def play(self):
+        self.game_engine.show_board()
+        # 무한반복
+        while(True):
+            # input row, col
+            row = int(input('행 : '))
+            col = int(input('열 : '))
+            # set row, col
+            self.game_engine.set(row,col)
+            # show board
+            self.game_engine.show_board()
+            # set winner
+            result = self.game_engine.set_winner()
+            # 승자가 있거나 무승부면, 게임오버, 결과 출력
+            if result == '': continue
+            print('무승부' if result=='d' else f'승자 : {result}')
+
+
+if __name__ == '__main__':
+    tic = TictactoeConsole()
+    tic.play()
+
 # show_board
 # input row, col
 # set row, col
--- tictactoe/tictactoe_gameengine.py
+++ tictactoe/tictactoe_gameengine.py
@@ -4,20 +4,11 @@ class TictactoeGameEngine:
         self.turn = 'X'
 
     def show_board(self):
-        # 내가 짠 코드
-        for i in range(len(self.board)):
-            print((self.board[i]+"   ") if i%3!=2 else (self.board[i]+"\n"), end='')
-        
-        # 조별로 짠 코드
-        for i in range(0,6+1,3):
-            print('  '.join(self.board[i:i+3]))
-
         # 풀이
         for i, v in enumerate(self.board):
             print(v+' ', end='')
             if i % 3==2:
                 print()
-        print()
 
     def set(self, row, col):
         self.board[3*(row-1)+(col-1)] = self.turn
@@ -30,41 +21,31 @@ class TictactoeGameEngine:
         for row in range(1,3+1):
             if self.board[self.position_to_index(row,1)]\
             == self.board[self.position_to_index(row,2)]\
-            == self.board[self.position_to_index(row,3)]:
+            == self.board[self.position_to_index(row,3)]\
+            == self.turn :
                 return self.turn
             if self.board[self.position_to_index(1,row)]\
             == self.board[self.position_to_index(2,row)]\
-            == self.board[self.position_to_index(3,row)]:
+            == self.board[self.position_to_index(3,row)]\
+            == self.turn :
                 return self.turn
 
         # 대각선
         if self.board[self.position_to_index(1, 3)] \
         == self.board[self.position_to_index(2, 2)] \
-        == self.board[self.position_to_index(3, 1)]:
+        == self.board[self.position_to_index(3, 1)]\
+        == self.turn:
             return self.turn
         if self.board[self.position_to_index(1, 1)] \
         == self.board[self.position_to_index(2, 2)] \
-        == self.board[self.position_to_index(3, 3)]:
+        == self.board[self.position_to_index(3, 3)]\
+        == self.turn:
             return self.turn
 
         # , 더이상 놓을 자리가 없음: self.board에 빈칸이 없음: self.board에 '.' 없음
         if not '.' in self.board:
             return 'd'  # draw
+        return ''
 
     def change_turn(self):
         self.turn = 'O' if self.turn=='X' else 'X'
-
-if __name__ == '__main__':
-    game_engin = TictactoeGameEngine()
-    game_engin.show_board()     # ...\n...\n...
-    game_engin.set(2,2)
-    game_engin.show_board()     #['.','.','.','.','X','.','.','.']
-    game_engin.set(2,1)
-    game_engin.set(2,3)
-    game_engin.show_board()     #['.','.','.','.'X','X','X','.','.']
-    print(game_engin.set_winner() )    # '-' 아무것도 안뜨게
-    print(game_engin.turn)  # 'O'   현재 턴
-    game_engin.change_turn()
-    print(game_engin.turn)      # 'O'   현재 턴
-    game_engin.change_turn()
-    print(game_engin.turn)  # 'O'   현재 턴
