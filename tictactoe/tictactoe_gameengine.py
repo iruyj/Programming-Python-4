b25e04148c52e0a0b4af305b033bcd2214002202
Jyuri <jeon1310@gmail.com>
2021-11-05 14:52:23 +0900

    tictactoe game engine winner 수정 후 완성

--- tictactoe/tictactoe_gameengine.py
+++ tictactoe/tictactoe_gameengine.py
@@ -31,46 +31,25 @@ class TictactoeGameEngine:
             if self.board[self.position_to_index(row,1)]\
             == self.board[self.position_to_index(row,2)]\
             == self.board[self.position_to_index(row,3)]:
-                return '-'
+                return self.turn
             if self.board[self.position_to_index(1,row)]\
             == self.board[self.position_to_index(2,row)]\
             == self.board[self.position_to_index(3,row)]:
-                return '|'
+                return self.turn
 
         # 대각선
         if self.board[self.position_to_index(1, 3)] \
         == self.board[self.position_to_index(2, 2)] \
         == self.board[self.position_to_index(3, 1)]:
-            return '-'
+            return self.turn
         if self.board[self.position_to_index(1, 1)] \
         == self.board[self.position_to_index(2, 2)] \
         == self.board[self.position_to_index(3, 3)]:
-            return '|'
+            return self.turn
 
-        # 직접 짠 코드 ---------------------------------
-        # - 3줄
-        # for i in range(0, len(self.board), 3):
-        #     if (self.board[i]==self.board[i+1]==self.board[i+2]):
-        #         if(self.board[i]!='.'):
-        #             print('-',self.board[i])
-        # # | 3줄
-        # for i in range(0,3):
-        #     if (self.board[i]==self.board[i+3]==self.board[i+6]):
-        #         if(self.board[i]!='.'):
-        #             print('|',self.board[i])
-        #
-        # if self.board[2] == self.board[4] == self.board[6]:     # /
-        #     return print(f'${self.board[2]} 승리')
-        #
-        # if self.board[0] == self.board[4] == self.board[8]:     # \
-        #     return print(f'${self.board[0]} 승리')
-
-        # 무승부: 위의 조건 다 통과   ------------------------
-        for i in self.board:
-            if i == '.':
-                return '무승부 아님'
         # , 더이상 놓을 자리가 없음: self.board에 빈칸이 없음: self.board에 '.' 없음
-        return 'd'  # draw
+        if not '.' in self.board:
+            return 'd'  # draw
 
     def change_turn(self):
         self.turn = 'O' if self.turn=='X' else 'X'
@@ -83,7 +62,7 @@ if __name__ == '__main__':
     game_engin.set(2,1)
     game_engin.set(2,3)
     game_engin.show_board()     #['.','.','.','.'X','X','X','.','.']
-    game_engin.set_winner()     # '-' 아무것도 안뜨게
+    print(game_engin.set_winner() )    # '-' 아무것도 안뜨게
     print(game_engin.turn)  # 'O'   현재 턴
     game_engin.change_turn()
     print(game_engin.turn)      # 'O'   현재 턴
